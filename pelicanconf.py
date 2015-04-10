5806477a3d933dd32599127291bcb73915725e31
Seth Fischer <seth@fischer.nz>
2015-04-10 21:31:11 +1200

    Define CATEGORIES_URL

--- pelicanconf.py
+++ pelicanconf.py
@@ -16,6 +16,7 @@ TIMEZONE = 'Pacific/Auckland'
 DEFAULT_LANG = u'en'
 
 TAGS_URL = 'tags.html'
+CATEGORIES_URL = 'categories.html'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
