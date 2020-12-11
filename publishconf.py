34836259fff25c6e4b5e044a9d5f58571fc085e5
Seth Fischer <seth@fischer.nz>
2020-12-11 16:59:37 +1300

    HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = "http://seth.fischer.nz"
+SITEURL = "https://seth.fischer.nz"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
